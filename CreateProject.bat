fba0a57c7d59adb59fce48ec42beb2180cb7c9f8
Viazel <timeo.morsilli@gmail.com>
2022-09-08 19:50:34 +0200

    Premake

--- CreateProject.bat
+++ CreateProject.bat
@@ -1,2 +1,2 @@
-C:\Users\Code\C++\LearningVulkan\vendor\bin\premake\premake5.exe vs2019
+vendor\bin\premake\premake5.exe vs2019
 PAUSE
--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 workspace "Hazel"
 	architecture "x64"
 
+	startproject "Sandbox"
+
 	configurations
 	{
 		"Debug",
